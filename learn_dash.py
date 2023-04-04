102d97ff2dcfb9e70fa33f5dd006647fe0bdcc96
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-04-04 09:58:02 +0700

    trying to get dash running

---
+++ learn_dash.py
@@ -0,0 +1,11 @@
+# from dash import Dash,html
+
+def main() -> None:
+    print("Hello World")
+    # app = Dash() 
+    # app.title = 'Medal dashboard'
+    # app.layout =  html.Div()
+    # app.run()
+
+if __name__ == "__main__":
+    main()
