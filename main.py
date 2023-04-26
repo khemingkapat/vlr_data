232fe046ac288307726217a7833e2feb3974a6fd
KhemIngkapat <60930364+KhemIngkapat@users.noreply.github.com>
2023-04-26 20:36:07 +0700

    fix importing error

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import httpx
 from selectolax.parser import HTMLParser
 import re
 import pandas as pd
-from classes import Event,Match
+from classes import Event, Match
 
 
-base_url = 'https://www.vlr.gg'
+base_url = "https://www.vlr.gg"
 
-event_url = '/event/matches/1188/champions-tour-2023-lock-in-s-o-paulo/?series_id=all'
+event_url = "/event/matches/1188/champions-tour-2023-lock-in-s-o-paulo/?series_id=all"
 
 url = base_url + event_url
 
 lock_in = Event(url)
 
-lock_in.get_matches(3)
+for match in lock_in.get_matches(3):
+    print(match.get_scoreboard())
